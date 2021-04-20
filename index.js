9770348b2f5619dce82fa2d5ad00922df076d6f1
Mirkhat <MirkhatA@users.noreply.github.com>
2021-04-20 17:45:05 +0600

    navbar commit
    created navbar layout with all pages of the project

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.engine('hbs', hbs.engine) //register hbs in engine
 app.set('view engine', 'hbs') //use it
 app.set('views', 'views')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
