20c92dd06b3a446ed0996afded23323aa18a2b9d
Mirkhat <MirkhatA@users.noreply.github.com>
2021-05-10 19:22:12 +0600

    order commit
    order page was created, all orders arr in db and displayed on order page

--- routes/orders.js
+++ routes/orders.js
@@ -3,10 +3,26 @@ const Order = require('../models/order')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    res.render('orders', {
-        isOrder: true,
-        title: 'Orders'
-    })
+    try {
+        // list of orders
+        const orders = await Order.find({'user.userId': req.user._id})
+        .populate('user.userId')
+
+        res.render('orders', {
+            isOrder: true,
+            title: 'Orders',
+            orders: orders.map(o => {
+                return {
+                    ...o._doc,
+                    price: o.foods.reduce((total, c) => {
+                        return total += c.count * c.food.price
+                    }, 0)
+                }
+            })
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 
@@ -17,12 +33,12 @@ router.post('/', async (req, res) => {
         .populate('cart.items.foodId')
         .execPopulate();
     
-        const foods = user.cart.items.map(i => ({
+        const food = user.cart.items.map(i => ({
             count: i.count,
             food: {...i.foodId._doc}
         }))
     
-        const order = new Order({
+        const orders = new Order({
             user: {
                 name: req.user.name,
                 userId: req.user
